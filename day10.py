1153daf2217329be14c7b1d40d30c9415d0cad79
Jeffrey Borlik <jborlik@gmail.com>
2019-12-30 11:00:14 -0800

    Completed Day 10 part 2

--- day10.py
+++ day10.py
@@ -2,7 +2,8 @@
 #import numpy
 #import copy
 #from IntCodeProgram import IntCodeProgram
-
+import math
+from blist import blist
 
 with open('day10.dat') as datafile:
     asteroidmap = [x.strip() for x in datafile.readlines()]
@@ -36,6 +37,36 @@ for iy, row in enumerate(asteroidmap):
         if cell == '#':
             asteroids[(ix,iy)] = 0
 
+def isAsteroidVisible(aRoid, baseRoid):
+    isx = 0
+    isy = 1
+    rise = aRoid[isy] - baseRoid[isy]
+    run  = aRoid[isx] - baseRoid[isx]
+    if run == 0:
+        # flip axes so that we can look straight up and down too
+        isx = 1
+        isy = 0
+        rise = aRoid[isy] - baseRoid[isy]
+        run  = aRoid[isx] - baseRoid[isx]
+    rundir = int(run / abs(run)) # 1=check>base, -1=check<base
+    iblocks = 0
+    for ixinc in range(1*rundir,run,rundir):
+        yloc = rise/run*(ixinc) + baseRoid[isy]
+        remainder = yloc - int(yloc)
+        if (abs(remainder) < 1e-5):
+            blockloc = (baseRoid[0] + ixinc,
+                        int(yloc))
+            if isx == 1:
+                blockloc = (int(yloc),baseRoid[isx] + ixinc)
+            if blockloc in asteroids:
+                iblocks += 1
+#                        print(f" blocked by {blockloc}")
+                break
+    if iblocks == 0:
+        return True
+    return False
+
+
 # for each asteroid determine the count of visible
 for base_asteroid, count in asteroids.items():
     # count the visible
@@ -43,32 +74,7 @@ for base_asteroid, count in asteroids.items():
 #    print(f"ASTEROID {base_asteroid}:")
     for check_asteroid in asteroids.keys():
         if base_asteroid != check_asteroid:
-#            print(f"  checking asteroid {check_asteroid}", end='')
-            isx = 0
-            isy = 1
-            rise = check_asteroid[isy] - base_asteroid[isy]
-            run  = check_asteroid[isx] - base_asteroid[isx]
-            if run == 0:
-                # flip axes so that we can look straight up and down too
-                isx = 1
-                isy = 0
-                rise = check_asteroid[isy] - base_asteroid[isy]
-                run  = check_asteroid[isx] - base_asteroid[isx]
-            rundir = int(run / abs(run)) # 1=check>base, -1=check<base
-            iblocks = 0
-            for ixinc in range(1*rundir,run,rundir):
-                yloc = rise/run*(ixinc) + base_asteroid[isy]
-                remainder = yloc - int(yloc)
-                if (abs(remainder) < 1e-5):
-                    blockloc = (base_asteroid[0] + ixinc,
-                                int(yloc))
-                    if isx == 1:
-                        blockloc = (int(yloc),base_asteroid[isx] + ixinc)
-                    if blockloc in asteroids:
-                        iblocks += 1
-#                        print(f" blocked by {blockloc}")
-                        break
-            if iblocks == 0:
+            if isAsteroidVisible(check_asteroid,base_asteroid):
                 count += 1
 #                print(f"... visible!  count={count}")
     asteroids[base_asteroid] = count
@@ -82,7 +88,38 @@ for aasteroid,count in asteroids.items():
 
 print(f"Maxcount Asteroid at {maxcount_roid} has count={maxcount}")            
 
+base_location = maxcount_roid
+asteroids.pop(base_location)
 
+minangle = math.atan2(0-height, 0-width) - math.atan2(0-height, 1-width)
+maxdist = math.sqrt(height**2 + width**2)
+
+def calcAngle(location):
+    global base_location
+    ydist = location[1] - base_location[1]
+    xdist = location[0] - base_location[0]
+    ang = math.atan2(ydist,xdist) + math.pi/2
+    if ang < 0:
+        ang += 2*math.pi
+    # need a tiebreaker... furthest away should go first
+    totdist = math.sqrt(ydist**2 + xdist**2)
+    penalty = totdist / maxdist * abs(minangle)
+    ang -= penalty
+    return ang
     
+
+remaining_roids = blist(sorted(asteroids.keys(), key=calcAngle))
+
+count_destroyed = 0
+last_destroyed = None
+while count_destroyed < len(remaining_roids):
+    for i,aRoid in enumerate(remaining_roids):
+        if aRoid != None:
+            if isAsteroidVisible(aRoid,base_location):
+                # kill it
+                last_destroyed = aRoid
+                remaining_roids[i] = None
+                count_destroyed += 1
+                print(f"Destroyed {count_destroyed} at {last_destroyed}")
 
 
